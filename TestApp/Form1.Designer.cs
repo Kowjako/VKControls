951bfc608a5984c5f964eb34399a84f12381ce4c
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-11-30 22:03:52 +0100

    Start implementing for textbox

--- TestApp/Form1.Designer.cs
+++ TestApp/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace TestApp
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.flatButton1 = new VKControls.Buttons.FlatButton();
+            this.flatTextBox1 = new VKControls.TextBox.FlatTextBox();
             this.SuspendLayout();
             // 
             // flatButton1
@@ -39,9 +40,9 @@ namespace TestApp
             this.flatButton1.ButtonImage = ((System.Drawing.Image)(resources.GetObject("flatButton1.ButtonImage")));
             this.flatButton1.ButtonName = "Paragony";
             this.flatButton1.ButtonNameColor = System.Drawing.Color.White;
-            this.flatButton1.ButtonNameFont = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatButton1.ButtonNameFont = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.flatButton1.FirstGradientColor = System.Drawing.Color.Blue;
-            this.flatButton1.Location = new System.Drawing.Point(74, 67);
+            this.flatButton1.Location = new System.Drawing.Point(29, 12);
             this.flatButton1.MaximumSize = new System.Drawing.Size(200, 170);
             this.flatButton1.MinimumSize = new System.Drawing.Size(200, 170);
             this.flatButton1.Name = "flatButton1";
@@ -49,11 +50,19 @@ namespace TestApp
             this.flatButton1.Size = new System.Drawing.Size(200, 170);
             this.flatButton1.TabIndex = 0;
             // 
+            // flatTextBox1
+            // 
+            this.flatTextBox1.Location = new System.Drawing.Point(55, 232);
+            this.flatTextBox1.Name = "flatTextBox1";
+            this.flatTextBox1.Size = new System.Drawing.Size(284, 38);
+            this.flatTextBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flatTextBox1);
             this.Controls.Add(this.flatButton1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -64,6 +73,7 @@ namespace TestApp
         #endregion
 
         private VKControls.Buttons.FlatButton flatButton1;
+        private VKControls.TextBox.FlatTextBox flatTextBox1;
     }
 }
 
---
+++ VKControls/TextBox/FlatTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VKControls.TextBox
+{
+    public partial class FlatTextBox : UserControl
+    {
+        public FlatTextBox()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor |
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.ResizeRedraw |
+                ControlStyles.UserPaint, true);
+            BackColor = Color.Transparent;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+    }
+}
