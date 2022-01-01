6d78a73e1b7e001dc497f6f1aed047652715c019
Uladzimir Kaviaka <kowyako@yandex.ru>
2022-01-01 14:21:01 +0100

    Start implementing VKPanelHeader

---
+++ VKControls/VKPanelHeader/VKPanelHeader.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace VKControls.VKPanelHeader
+{
+    partial class VKPanelHeader
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VKPanelHeader));
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.bClose = new System.Windows.Forms.PictureBox();
+            this.bMinimize = new System.Windows.Forms.PictureBox();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bMinimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.headerLabel);
+            this.mainPanel.Controls.Add(this.bClose);
+            this.mainPanel.Controls.Add(this.bMinimize);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(692, 50);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // bClose
+            // 
+            this.bClose.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bClose.BackColor = System.Drawing.Color.Transparent;
+            this.bClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bClose.Image = ((System.Drawing.Image)(resources.GetObject("bClose.Image")));
+            this.bClose.Location = new System.Drawing.Point(638, 0);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(50, 50);
+            this.bClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bClose.TabIndex = 1;
+            this.bClose.TabStop = false;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // bMinimize
+            // 
+            this.bMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.bMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bMinimize.Image = ((System.Drawing.Image)(resources.GetObject("bMinimize.Image")));
+            this.bMinimize.Location = new System.Drawing.Point(583, 0);
+            this.bMinimize.Name = "bMinimize";
+            this.bMinimize.Size = new System.Drawing.Size(50, 50);
+            this.bMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bMinimize.TabIndex = 0;
+            this.bMinimize.TabStop = false;
+            this.bMinimize.Click += new System.EventHandler(this.bMinimize_Click);
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Location = new System.Drawing.Point(22, 19);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(90, 13);
+            this.headerLabel.TabIndex = 2;
+            this.headerLabel.Text = "Application Name";
+            // 
+            // VKPanelHeader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mainPanel);
+            this.Name = "VKPanelHeader";
+            this.Size = new System.Drawing.Size(692, 50);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bMinimize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.PictureBox bMinimize;
+        private System.Windows.Forms.PictureBox bClose;
+        private System.Windows.Forms.Label headerLabel;
+    }
+}
---
+++ VKControls/VKPanelHeader/VKPanelHeader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VKControls.VKPanelHeader
+{
+    public partial class VKPanelHeader : UserControl
+    {
+        public VKPanelHeader()
+        {
+            InitializeComponent();
+        }
+
+        public Color HeaderColor { get; set; } = Color.Black;
+
+        public string HeaderText { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            mainPanel.BackColor = HeaderColor;
+            headerLabel.Text = HeaderText;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+        }
+
+        private void bMinimize_Click(object sender, EventArgs e)
+        {
+            ParentForm.WindowState = FormWindowState.Minimized;
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            ParentForm.Close();
+        }
+    }
+}
